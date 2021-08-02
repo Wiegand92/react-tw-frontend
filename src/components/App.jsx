8151b515a39ea36b7c3264ec190cb46e270e97f6
wiegand92 <wiegand.alex@gmail.com>
2021-08-02 11:55:24 -0400

    configure eslint/prettier to autoformat on save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const App = () => (
+const App = () => {
+  return (
     <div className="app">
       <h1>Hello World!</h1>
       <p>
-        Check out the README 
-        <a href='https://github.com/Wiegand92/react-tw'>here</a>
+        Check out the README
+        <a href="https://github.com/Wiegand92/react-tw">here</a>
       </p>
     </div>
-);  
+  );
+};
 
 export default App;

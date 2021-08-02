6a837e9c84ce10983c8429c8cc1762b4d2e40f40
wiegand92 <wiegand.alex@gmail.com>
2021-08-02 11:34:29 -0400

    configure eslint to work with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
+  extends: ['prettier'],
+  plugins: ['prettier'],
   parser: 'babel-eslint',
   env: {
     browser: true,
     es6: true,
+  },
+  rules: {
+    'prettier/prettier': ['warn', {'endOfLine': 'auto'}]
   },
   settings: {
     react: {
